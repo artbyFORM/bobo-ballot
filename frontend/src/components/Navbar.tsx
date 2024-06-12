12c28a149bf947393e2c371ea21c8ef8fe86539f
Erik Martin <techgeek1129@gmail.com>
2024-06-12 15:53:17 -0700

    dark mode toggle icon

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
 import { makeStyles } from '@mui/styles';
 import { Link } from 'react-router-dom';
 import { useTheme } from '../ThemeContext';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 const useStyles = makeStyles(() => ({
   link: {
@@ -27,9 +30,9 @@ return (
             <Link to="/vote" className={classes.link}>
                 <Button color="inherit" className="text-white">Vote</Button>
             </Link>
-            <Button color="inherit" onClick={toggleTheme}>
-                {isDarkMode ? "Dark" : "Light"}
-            </Button>
+            <IconButton color="inherit" onClick={toggleTheme}>
+                {isDarkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
         </Toolbar>
     </AppBar>
 );
