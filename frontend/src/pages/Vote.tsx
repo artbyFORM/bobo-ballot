8d24d1056a73f05efbd1c36c47d06a4c65d6b13a
Erik Martin <techgeek1129@gmail.com>
2024-06-17 16:34:18 -0700

    refactor vote buttons

--- frontend/src/pages/Vote.tsx
+++ frontend/src/pages/Vote.tsx
@@ -5,16 +5,20 @@ import { useEffect, useState } from 'react';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
+const validVoteKeys = [1, 2, 3, 4, 5]; //TODO: make this based on current round
+
 const Vote: React.FC = () => {
   const [row, setRow] = useState<number>(1);
   const totalRows = 12; //hard coded for now
-  const [vote, setVote] = useState('1');
+
+  const [vote, setVote] = useState<number>(1);
 
   // handle keyboard input
   useEffect(() => {
       const handleKeyPress = (event: KeyboardEvent) => {
-          if (event.key === '1' || event.key === '2' || event.key === '3') {
-              handleSubmit(event.key);
+          const key = Number(event.key);
+          if (validVoteKeys.includes(key) && key !== vote){
+            submitVote(key);
           }
       };
 
@@ -23,13 +27,13 @@ const Vote: React.FC = () => {
       return () => {
           document.removeEventListener('keydown', handleKeyPress);
       };
-  }, []);
+  });
 
   const changeRow = (newRow: number) => {
     setRow(newRow);
   };
 
-  const handleSubmit = async (vote: string) => {
+  const submitVote = async (vote: number) => {
     setVote(vote);
   };
 
@@ -60,9 +64,9 @@ const Vote: React.FC = () => {
                   {row ? <h1 className="text-4xl font-light pb-5">{"Skrillex & Wolfgang Gartner"}</h1> : "..."}
                   <div className="w-10 h-50"/>
                   <div className="flex pb-5">
-                      <button className={`rounded-full text-4xl size-24 px-5 mr-10 ${vote === '1' ? 'bg-pink-500 text-white' : 'bg-slate-100 text-black'}`} onClick={() => handleSubmit('1')}>1</button>
-                      <button className={`rounded-full text-4xl size-24 px-5 mr-10 ${vote === '2' ? 'bg-pink-500 text-white' : 'bg-slate-100 text-black'}`} onClick={() => handleSubmit('2')}>2</button>
-                      <button className={`rounded-full text-4xl size-24 px-5 mr-10 ${vote === '3' ? 'bg-pink-500 text-white' : 'bg-slate-100 text-black'}`} onClick={() => handleSubmit('3')}>3</button>
+                      {validVoteKeys.map((key) => (
+                            <button className={`rounded-full text-4xl size-24 px-5 mr-10 ${vote === Number(key) ? 'bg-pink-500 text-white' : 'bg-slate-100 text-black'}`} onClick={() => submitVote(Number(key))}>{key}</button>
+                      ))}
                   </div>
               </div>
           </div>
