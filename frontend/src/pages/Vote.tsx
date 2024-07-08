1ff2a7928c0697357d83dca6586479765de0b7e7
Erik Martin <techgeek1129@gmail.com>
2024-07-07 19:26:38 -0700

    pull songs from june api

--- frontend/src/pages/Vote.tsx
+++ frontend/src/pages/Vote.tsx
@@ -5,14 +5,36 @@ import Waveform from '../components/Waveform';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
+import axios from 'axios';
+
 const validVoteKeys = [1, 2, 3, 4, 5]; // TODO: make this based on current round
 
 const Vote: React.FC = () => {
-  const [row, setRow] = useState<number>(1);
-  const totalRows = 12; // hard coded for now
+  const [id, setId] = useState<number>(103);
+  const totalRows = 999; // hard coded for now
   const [vote, setVote] = useState<number>(1);
+  const [data, setData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
-  const [trackURL, setTrackURL] = useState("https://s3.minecraft.college/party-rock-high-quality.mp3");
+  useEffect(() => {
+    async function getSong() {
+        const admkey = process.env.REACT_APP_ADMIN_KEY;
+        if (!admkey) return;
+        
+        const info = await axios.get(`https://api.submit.artbyform.com/admin/song/${id}`, {
+            headers: { Authorization: "Bearer " + admkey },
+            validateStatus: () => true,
+        });
+
+        //if (!info.data.success) return setErr(info.data.message);
+        setData(info.data.data);
+        console.log(info.data.data.listen);
+        setLoading(false);
+    }
+
+    getSong();
+  }, [id]);
+
 
   // Handle keyboard input
   useEffect(() => {
@@ -30,26 +52,28 @@ const Vote: React.FC = () => {
     };
   }, [vote]); // Added dependency array
 
-  const changeRow = (newRow: number) => {
-    setRow(newRow);
+  const changeId = (newRow: number) => {
+    setId(newRow);
   };
 
   const submitVote = async (vote: number) => {
     setVote(vote);
   };
 
+  if (loading) return <div>Loading...</div>;
+
   return (
     <div className='p-10'>
       <div className="flex justify-between items-center p-15 w-full h-full">
-        <IconButton className="size-5 ml-10" onClick={() => changeRow(row - 1)} disabled={row === 1}><ArrowBackIcon /></IconButton>
+        <IconButton className="size-5 ml-10" onClick={() => changeId(id - 1)} disabled={id === 1}><ArrowBackIcon /></IconButton>
         <div className="flex justify-center items-center p-15 w-full h-full">
           <div className="flex flex-col items-center space-y-15">
-            <h1 className="text-xl font-bold">{`${row}/${totalRows}`}</h1>
-            {row ? <h1 className="text-4xl font-extrabold pt-5 pb-5">{"Bangarang (feat. Sirah)"}</h1> : "..."}
-            {row ? <h1 className="text-4xl font-light pb-5">{"Skrillex"}</h1> : "..."}
-            {row ? (
+            <h1 className="text-xl font-bold">{`${id}/${totalRows}`}</h1>
+            {id ? <h1 className="text-4xl font-extrabold pt-5 pb-5">{data.song.data.title}</h1> : "..."}
+            {id ? <h1 className="text-4xl font-light pb-5">{data.artists[0].data.name}</h1> : "..."}
+            {id ? (
               <div className="w-900 h-200 pt-5 pb-10">
-                <Waveform key={`waveform-${row}`} url={trackURL} />
+                <Waveform key={`waveform-${id}`} url={data.listen} />
               </div>
             ) : null}
             <div className="flex pb-5">
@@ -59,7 +83,7 @@ const Vote: React.FC = () => {
             </div>
           </div>
         </div>
-        <IconButton className="size-5 mr-10" onClick={() => changeRow(row + 1)} disabled={row === totalRows}><ArrowForwardIcon /></IconButton>
+        <IconButton className="size-5 mr-10" onClick={() => changeId(id + 1)} disabled={id === totalRows}><ArrowForwardIcon /></IconButton>
       </div>
     </div>
   );
